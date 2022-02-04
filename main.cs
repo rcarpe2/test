a03e5b2ad9b47a5b1a996d4f0d85fb3a13e0a142
Roger Carpenter <>
2022-02-04 13:55:10 +0000

    q2 stuff

--- main.cs
+++ main.cs
@@ -2,6 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("this is the original program");
+    Console.WriteLine ("this is the answer to q2");
   }
 }
